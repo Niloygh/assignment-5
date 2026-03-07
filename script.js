9a25c77b3d2a23a30477bb019770749f8297892b
Niloygh <niloyniloy843@gmail.com>
2026-03-07 17:04:21 +0600

    login section complete and home section navber or button make

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('login-btn')
+.addEventListener('click', function(){
+    const username = document.getElementById('input-Username')
+    const usernameValue = username.value;
+    console.log(usernameValue)
+
+    const password = document.getElementById('input-password')
+    const passwordValue = password.value;
+    console.log(passwordValue)
+
+    if(usernameValue === 'admin' & passwordValue === 'admin123'){
+        alert('Login Successful')
+        window.location.assign('home.html')
+    }
+    else{
+        alert('Login Failed')
+        return;
+    }
+})
