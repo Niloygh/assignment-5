8e3a7441e60cf4eb0af283c38ae85040a0de8a45
Niloygh <niloyniloy843@gmail.com>
2026-03-08 22:40:27 +0600

    show all, open or closed  button card show

--- script/home.js
+++ script/home.js
@@ -1,3 +1,5 @@
+let currentStatus = "all";
+
 
 const allBtn = document.getElementById('all-btn')
 const openBtn = document.getElementById('open-btn')
@@ -8,21 +10,60 @@ const count = document.getElementById('count')
 
 const loadingSpinner = document.getElementById('loadingSpinner')
 
+allBtn.addEventListener('click', () => {
+    cardFilters('all')
+    currentStatus = 'all'
+    switchTab('all-btn');
+})
+openBtn.addEventListener('click', () => {
+    cardFilters('open')
+    currentStatus = 'open'
+    switchTab('open-btn');
+})
+closedBtn.addEventListener('click', () => {
+    cardFilters('closed')
+    currentStatus = 'closed'
+    switchTab('closed-btn');
+})
 
 
 
-
-allBtn.classList.remove('bg-white', 'color-gray')
+,closedBtn.classList.remove('bg-white', 'color-gray')
 allBtn.classList.add('color-primary')
 // console.log(allBtn, openBtn, closedBtn)
 
+const updateTotalCard = () => {
+    const visibleCards = document.querySelectorAll("#card-container .card");
+    let sum = 0;
+    visibleCards.forEach((card) => {
+        if (card.style.display !== "none") sum++;
+    });
+    count.innerText = sum;
+};
 
+function cardFilters(status) {
 
-function showLoading(){
+    const divEl = document.querySelectorAll('#card-container .card')
+    // console.log(divEl)
+
+    divEl.forEach((i) => {
+        if (status === 'all') {
+            i.style.display = 'block'
+        }
+        else {
+            i.style.display = i.dataset.status === status ? 'block' : 'none'
+
+        }
+    })
+    updateTotalCard()
+    console.log(updateTotalCard())
+}
+
+function showLoading() {
     loadingSpinner.classList.remove('hidden')
     cardContainer.innerHTML = "";
 }
-function hideLoading(){
+function hideLoading() {
     loadingSpinner.classList.add('hidden')
 }
 
@@ -42,6 +83,9 @@ function switchTab(tab) {
 
         allBtn.classList.remove('bg-white', 'color-gray')
         allBtn.classList.add('color-primary')
+
+        // cardFilters('all')
+
     }
     if (tab === 'open-btn') {
         closedBtn.classList.add('bg-white', 'color-gray')
@@ -53,6 +97,9 @@ function switchTab(tab) {
 
         openBtn.classList.remove('bg-white', 'color-gray')
         openBtn.classList.add('color-primary')
+
+        // cardFilters('open')
+
     }
     if (tab === 'closed-btn') {
         allBtn.classList.add('bg-white', 'color-gray')
@@ -64,9 +111,14 @@ function switchTab(tab) {
 
         closedBtn.classList.remove('bg-white', 'color-gray')
         closedBtn.classList.add('color-primary')
+
+        // cardFilters('closed')
+
     }
 
 }
+
+
 
 
 async function allLoadCards() {
@@ -82,12 +134,12 @@ async function openCard() {
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     const data = await res.json();
 
-    if(data.data.status === 'open'){
-        
+    if (data.data.status === 'open') {
+
     }
-    
+
     hideLoading()
-    
+
 }
 
 
@@ -95,16 +147,17 @@ async function openCard() {
 function displayCard(cards) {
     // console.log(cards)
     count.innerText = cards.length
-    const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
-    
+
     cards.forEach(card => {
-        console.log(card.labels)
-        
+        // console.log(card.labels)
+
+
+
         // count = card.length
         //     {
-            // "id": 48,
-            // "title": "Browser console shows warnings",
+        // "id": 48,
+        // "title": "Browser console shows warnings",
         // "description": "Multiple deprecation warnings appearing in browser console. Need to update deprecated code.",
         // "status": "open",
         // "labels": [
@@ -120,10 +173,17 @@ function displayCard(cards) {
         // console.log(card.labels)
 
         const div = document.createElement("div");
-        div.classList.add('shadow-sm')
+
+        // div.dataset.status = card.status
+
+
+        // div.classList.add('card shadow-sm')
+        div.className = 'card shadow-sm'
+        div.dataset.status = card.status
+
         div.innerHTML = `
-        <div class="h-1 rounded-t-2xl ${card.status === 'open'? 'color-green' : 'color-primary'}"></div>
-        <div class='card card-body space-y-3'>
+        <div class="h-1 rounded-t-2xl ${card.status === 'open' ? 'color-green' : 'color-primary'}"></div>
+        <div class=' card-body space-y-3'>
         <div class="flex justify-between">
                     <img src="${card.status === 'open' ? 'assets/Open-Status.png' : 'assets/Closed- Status .png'}" alt="">
                     <h2 class="orange w-20 text-center rounded-full">${card.priority.toUpperCase()}</h2>
@@ -132,18 +192,18 @@ function displayCard(cards) {
                 <p>${card.description}</p>
                 <div class="flex gap-5">
 
-                    ${card.labels.map((item)=> 
+                    ${card.labels.map((item) =>
+            `
+                        <span class="${item === 'bug' ? 'orange' : item === 'help wanted' ? 'yellow' :
+                'color-primary'
+
+            }">${item === 'bug' ? 'BUG' :
+                item === 'help wanted' ? 'HELP WANTED' :
+                    item
+
+            }</span>
                         `
-                        <span class="${item === 'bug' ? 'orange' : item === 'help wanted' ? 'yellow':
-                            'color-primary'
-                            
-                        }">${item === 'bug' ? 'BUG' : 
-                            item === 'help wanted' ? 'HELP WANTED': 
-                            item
-                            
-                        }</span>
-                        `
-                    ).join("")}
+        ).join("")}
                 </div>
                 
                 <hr class= "color-gray">
@@ -156,6 +216,9 @@ function displayCard(cards) {
         `
 
         cardContainer.append(div)
+        cardFilters(currentStatus)
+        updateTotalCard()
+
 
 
 
